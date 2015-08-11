3877dfe984e1bea72ba11fb9740134b54065cfba
Rafael Gil <rafaelgil83@gmail.com>
2015-08-11 16:16:05 -0400

    updates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -51,13 +51,16 @@ Route::post('lookup', 'Change_OrgController@lookup');
 
       //probar este
 todo para los newhires ponerlos en canada, verificar si hay un grupo en AD para ellos
+todo hacer el formulario de payroll para los separations
 todo para separation el grupo de illyusa Sales no está funcionando para salir autodetectado, problemas con el javascript
+
 
 
 // STYLES
 todo- poner todos los label en negrita y pasar todo a labels
 todo- aplicar el estilo de newhire a separation (el required que no funciona en separation)
 todo- en los emails templates poner stylo para los nombres en negrita
+todo - arreglar que la firma salga siempre en la misma pag https://www.google.com/url?q=https%3A%2F%2Fcss-tricks.com%2Falmanac%2Fproperties%2Fp%2Fpage-break%2F&sa=D&sntz=1&usg=AFQjCNFlpwvW2m4q0_8VGFhbFcSIcO-hXQ
 
 
 
@@ -68,7 +71,10 @@ todo automatizado para cmabiar la signature y actualizar info en AD
 
 // SEPARATION
 todo- para el separation detectar si el campo de cellphone tiene algún valor is most likely the user has a cellphone and marcarlo en el listado
-
+todo - buscar por nombre y apellidos al mismo tiempo
+todo - quitar el enter en el formulario de buscar o decidir que hacer con él
+todo para los separation listar todos los directorios a los que el usuario está afiliado
+todo revisar que no hizo separation el mismo día a pesar que maren seleccionó el mismo día
 
 {"count":1,"0":{"sn":{"count":1,"0":"Gil"},"0":"sn","title":{"count":1,"0":"IT Infrastructure Engineer & Support"},"1":"title","givenname":{"count":1,"0":"Rafael"},"2":"givenname","memberof":{"count":15,"0":"CN=SlideShow_SecurityGrp_NA,OU=Security Groups,OU=Rye Brook,OU=North America,DC=ILLY-DOMAIN,DC=COM","1":"CN=HR-Tool,OU=Security Groups,OU=Rye Brook,OU=North America,DC=ILLY-DOMAIN,DC=COM","2":"CN=Wordpress-editor,OU=Security Groups,OU=Rye Brook,OU=North America,DC=ILLY-DOMAIN,DC=COM","3":"CN=si_infra_all,OU=Distribution,OU=Groups,OU=HeadQuarter,OU=Italy,DC=ILLY-DOMAIN,DC=COM","4":"CN=RoomUsersUSA,OU=Rooms,OU=New York City,OU=North America,DC=ILLY-DOMAIN,DC=COM","5":"CN=VNC Admin,OU=Service Groups,DC=ILLY-DOMAIN,DC=COM","6":"CN=PC Admins,OU=Service Groups,DC=ILLY-DOMAIN,DC=COM","7":"CN=illyusa Rye Brook Distribution Group,OU=Distribution Groups,OU=Rye Brook,OU=North America,DC=ILLY-DOMAIN,DC=COM","8":"CN=Report ServiceDesk IC Nord America,CN=Users,DC=ILLY-DOMAIN,DC=COM","9":"CN=Finance NA,OU=Security Groups,OU=Rye Brook,OU=North America,DC=ILLY-DOMAIN,DC=COM","10":"CN=VPN illy,OU=Security,OU=Groups,OU=HeadQuarter,OU=Italy,DC=ILLY-DOMAIN,DC=COM","11":"CN=Marketing NA,OU=Security Groups,OU=Rye Brook,OU=North America,DC=ILLY-DOMAIN,DC=COM","12":"CN=Information Technology NA,OU=Security Groups,OU=Rye Brook,OU=North America,DC=ILLY-DOMAIN,DC=COM","13":"CN=illyusaTeam Distribution Group,OU=Distribution Groups,OU=Rye Brook,OU=North America,DC=ILLY-DOMAIN,DC=COM","14":"CN=Wifi Employees,OU=Security,OU=Groups,OU=HeadQuarter,OU=Italy,DC=ILLY-DOMAIN,DC=COM"},"3":"memberof","department":{"count":1,"0":"IT"},"4":"department","company":{"count":1,"0":"illy caff\u00e8 North America, Inc."},"5":"company","samaccountname":{"count":1,"0":"gilra"},"6":"samaccountname","mail":{"count":1,"0":"Rafael.Gil@illy.com"},"7":"mail","manager":{"count":1,"0":"CN=Roy Forster,OU=Users,OU=Rye Brook,OU=North America,DC=ILLY-DOMAIN,DC=COM"},"8":"manager","count":9,"dn":"CN=Rafael Gil,OU=Users,OU=Rye Brook,OU=North America,DC=ILLY-DOMAIN,DC=COM"}}
 
