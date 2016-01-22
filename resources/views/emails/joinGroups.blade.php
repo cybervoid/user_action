1ec76463dcc8afca28c7bdb7f5cc5476f515d394
rafag <rafaelgil83@gmail.com>
2016-01-22 14:32:11 -0500

    - email template for new hire email creation request updated

--- resources/views/emails/joinGroups.blade.php
+++ resources/views/emails/joinGroups.blade.php
@@ -5,14 +5,13 @@ We need to add the following features for a new user:<br >
 
 <ul >
     <li >Create an email address</li >
-    <li >Add the user to the ticketing system</li >
+
 </ul >
 
 This is the information needed for the new user: <br >
 <ul >
     <li >Full name: {{ $name }}</li >
     <li >Username: {{ $userName }}</li >
-    <li >Manager: {{ $manager }}</li >
 </ul >
 <br >
 <br >
